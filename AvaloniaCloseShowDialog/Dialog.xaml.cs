3fb0cb81f7b388e17158b3003840654feea0508e
Silverfeelin <Silverfeelin@gmail.com>
2018-02-28 18:50:52 +0100

    File commit.

---
+++ AvaloniaCloseShowDialog/Dialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaCloseShowDialog
+{
+    public class Dialog : Window
+    {
+        Button btnClose;
+
+        public Dialog()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+            btnClose = this.FindControl<Button>("btnClose");
+            btnClose.Click += BtnClose_Click;
+        }
+
+        private void BtnClose_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ AvaloniaCloseShowDialog/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaCloseShowDialog
+{
+    public class MainWindow : Window
+    {
+        Button btnShow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            btnShow = this.FindControl<Button>("btnShow");
+            btnShow.Click += ShowDialog_Click;
+        }
+
+        private void ShowDialog_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            Dialog dialog = new Dialog();
+            dialog.ShowDialog();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
